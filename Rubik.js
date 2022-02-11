9d7f5def805af19a77dbea37967ecfd432ef36f4
dariush-hassani <dariush.hassani@outlook.com>
2022-02-11 17:27:43 +0330

    refactor

--- Rubik.js
+++ Rubik.js
@@ -16,22 +16,26 @@ let distance;
 let rotationDirection;
 let rotatingCubes;
 let materials = [];
-for (let i = 0; i <= 6; i++) {
-  let color;
-  if (i === 0) color = 0xff0000;
-  else if (i === 1) color = 0x00ff00;
-  else if (i === 2) color = 0x0000ff;
-  else if (i === 3) color = 0xffff00;
-  else if (i === 4) color = 0xffffff;
-  else if (i === 5) color = 0x0ffff0;
-  else if (i === 6) color = 0x000000;
-  materials.push(
-    new THREE.MeshPhongMaterial({
-      color: color,
-    })
-  );
-}
-let ungroup = (group, scene) => {
+
+const initMaterials = () => {
+  for (let i = 0; i <= 6; i++) {
+    let color;
+    if (i === 0) color = 0xff0000;
+    else if (i === 1) color = 0x00ff00;
+    else if (i === 2) color = 0x0000ff;
+    else if (i === 3) color = 0xffff00;
+    else if (i === 4) color = 0xffffff;
+    else if (i === 5) color = 0x0ffff0;
+    else if (i === 6) color = 0x000000;
+    materials.push(
+      new THREE.MeshPhongMaterial({
+        color: color,
+      })
+    );
+  }
+};
+
+const ungroup = (group, scene) => {
   let groupMatrix = group.matrix;
   let childs = [...group.children];
   for (let i = 0; i < childs.length; i++) {
@@ -114,6 +118,7 @@ const initFaces = (geometry, x, y, z) => {
 };
 
 const drawRubik = () => {
+  initMaterials();
   let geometry = new THREE.BoxGeometry(1, 1, 1);
   for (let x = -1; x <= 1; x++) {
     for (let y = -1; y <= 1; y++) {
@@ -159,7 +164,6 @@ const raycastMouseDown = (e) => {
     controls.enableRotate = false;
     startSelectedCube = firstCubeObject[0];
     startPoint = { x: mouse.x, y: mouse.y };
-    console.log(startSelectedCube);
   }
 };
 
@@ -205,33 +209,7 @@ const animateGroup = () => {
   }, 10);
 };
 
-const raycastMouseMove = (e) => {
-  if (startSelectedCube === undefined) return;
-  let mouse = { x: 0, y: 0 };
-  if (e.type === "mousemove") {
-    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
-  } else if (e.type === "touchmove") {
-    mouse.x = +(e.targetTouches[0].pageX / window.innerWidth) * 2 + -1;
-    mouse.y = -(e.targetTouches[0].pageY / window.innerHeight) * 2 + 1;
-  }
-
-  const raycaster = new THREE.Raycaster();
-  raycaster.setFromCamera(mouse, camera);
-  let intersects = raycaster.intersectObjects(scene.children, false);
-
-  let firstCubeObject = intersects.filter((x) => x.object.name !== "");
-
-  let dx = startSelectedCube.point.x - firstCubeObject[0]?.point.x;
-  let dy = startSelectedCube.point.y - firstCubeObject[0]?.point.y;
-  let dz = startSelectedCube.point.z - firstCubeObject[0]?.point.z;
-
-  let absX = Math.abs(dx);
-  let absY = Math.abs(dy);
-  let absZ = Math.abs(dz);
-
-  let limit = 0.3;
-
+const recognizeDirection = (dx, dy, dz, absX, absY, absZ, limit) => {
   if (startSelectedCube.point.x > 1.5) {
     if (absY > limit || absZ > limit) {
       if (absY > absZ) {
@@ -281,51 +259,62 @@ const raycastMouseMove = (e) => {
       }
     }
   }
+};
+
+const recognizeGroup = () => {
+  let group;
+  if (rotationDirection === "y" || rotationDirection === "-y") {
+    group = cubes.filter(
+      (x) => Math.abs(x.position.y - startSelectedCube.object.position.y) < 0.05
+    );
+  } else if (rotationDirection === "x" || rotationDirection === "-x") {
+    group = cubes.filter(
+      (x) => Math.abs(x.position.x - startSelectedCube.object.position.x) < 0.05
+    );
+  } else if (rotationDirection === "z" || rotationDirection === "-z") {
+    group = cubes.filter(
+      (x) => Math.abs(x.position.z - startSelectedCube.object.position.z) < 0.05
+    );
+  }
+  rotatingCubes = new THREE.Object3D();
+  group.forEach((x) => rotatingCubes.add(x));
+  scene.add(rotatingCubes);
+};
+
+const raycastMouseMove = (e) => {
+  if (startSelectedCube === undefined) return;
+  let mouse = { x: 0, y: 0 };
+  if (e.type === "mousemove") {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+  } else if (e.type === "touchmove") {
+    mouse.x = +(e.targetTouches[0].pageX / window.innerWidth) * 2 + -1;
+    mouse.y = -(e.targetTouches[0].pageY / window.innerHeight) * 2 + 1;
+  }
+
+  const raycaster = new THREE.Raycaster();
+  raycaster.setFromCamera(mouse, camera);
+  let intersects = raycaster.intersectObjects(scene.children, false);
+
+  let firstCubeObject = intersects.filter((x) => x.object.name !== "");
+
+  let dx = startSelectedCube.point.x - firstCubeObject[0]?.point.x;
+  let dy = startSelectedCube.point.y - firstCubeObject[0]?.point.y;
+  let dz = startSelectedCube.point.z - firstCubeObject[0]?.point.z;
+
+  let absX = Math.abs(dx);
+  let absY = Math.abs(dy);
+  let absZ = Math.abs(dz);
+
+  let limit = 0.3;
+
+  recognizeDirection(dx, dy, dz, absX, absY, absZ, limit);
   if (rotationDirection !== undefined && startSelectedCube !== undefined) {
-    let group;
-    if (rotationDirection === "y" || rotationDirection === "-y") {
-      group = cubes.filter(
-        (x) =>
-          Math.abs(x.position.y - startSelectedCube.object.position.y) < 0.05
-      );
-    } else if (rotationDirection === "x" || rotationDirection === "-x") {
-      group = cubes.filter(
-        (x) =>
-          Math.abs(x.position.x - startSelectedCube.object.position.x) < 0.05
-      );
-    } else if (rotationDirection === "z" || rotationDirection === "-z") {
-      group = cubes.filter(
-        (x) =>
-          Math.abs(x.position.z - startSelectedCube.object.position.z) < 0.05
-      );
-    }
-    rotatingCubes = new THREE.Object3D();
-    group.forEach((x) => rotatingCubes.add(x));
-    scene.add(rotatingCubes);
+    recognizeGroup();
     startSelectedCube = undefined;
     animateGroup();
   }
 };
-
-// const distanceCompute = (e) => {
-//   if (rotatingCubes === undefined) return;
-
-//   console.log(rotatingCubes);
-//   mouse = { x: 0, y: 0 };
-//   mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
-//   mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
-
-//   let vectorDistance = {
-//     x: mouse.x - startPoint.x,
-//     y: mouse.y - startPoint.y,
-//   };
-
-//   distance = Math.sqrt(
-//     Math.pow(vectorDistance.x, 2) + Math.pow(vectorDistance.y, 2)
-//   );
-
-//   console.log(distance);
-// };
 
 const animate = () => {
   requestAnimationFrame(animate);
@@ -333,25 +322,14 @@ const animate = () => {
   render();
 };
 
-window.addEventListener("mousedown", raycastMouseDown);
-window.addEventListener("mousemove", raycastMouseMove);
-// window.addEventListener("mousemove", distanceCompute);
-window.addEventListener("mouseup", () => {
+const resetValues = () => {
   controls.enableRotate = true;
   distance = undefined;
   rotationDirection = undefined;
   startSelectedCube = undefined;
-});
-window.addEventListener("touchstart", raycastMouseDown);
-document.getElementById("root").addEventListener("touchmove", raycastMouseMove);
-// window.addEventListener("mousemove", distanceCompute);
-window.addEventListener("touchend", () => {
-  controls.enableRotate = true;
-  distance = undefined;
-  rotationDirection = undefined;
-  startSelectedCube = undefined;
-});
-document.getElementById("reset-view").addEventListener("click", () => {
+};
+
+const resetView = () => {
   let currentPosition = camera.position;
   let targetPosition = { x: 5, y: 5, z: 5 };
   let duration = 2000;
@@ -377,35 +355,16 @@ document.getElementById("reset-view").addEventListener("click", () => {
       clearInterval(timer);
     }
   }, 10);
-});
-
-document.getElementById("reset-view").addEventListener("touchstart", () => {
-  let currentPosition = camera.position;
-  let targetPosition = { x: 5, y: 5, z: 5 };
-  let duration = 2000;
-  let stepX = ((targetPosition.x - currentPosition.x) / duration) * 10;
-  let stepY = ((targetPosition.y - currentPosition.y) / duration) * 10;
-  let stepZ = ((targetPosition.z - currentPosition.z) / duration) * 10;
-
-  let x = currentPosition.x;
-  let y = currentPosition.y;
-  let z = currentPosition.z;
-  let timer = setInterval(() => {
-    x += stepX;
-    y += stepY;
-    z += stepZ;
-    camera.position.x = x;
-    camera.position.y = y;
-    camera.position.z = z;
-    if (x <= 5.1 && x >= 4.9 && y <= 5.1 && y >= 4.9 && z <= 5.1 && z >= 4.9) {
-      camera.position.x = 5;
-      camera.position.y = 5;
-      camera.position.z = 5;
-      render();
-      clearInterval(timer);
-    }
-  }, 10);
-});
+};
 
 initialScene();
 animate();
+
+window.addEventListener("mousedown", raycastMouseDown);
+window.addEventListener("mousemove", raycastMouseMove);
+window.addEventListener("mouseup", resetValues);
+window.addEventListener("touchstart", raycastMouseDown);
+document.getElementById("root").addEventListener("touchmove", raycastMouseMove);
+window.addEventListener("touchend", resetValues);
+document.getElementById("reset-view").addEventListener("click", resetView);
+document.getElementById("reset-view").addEventListener("touchstart", resetView);
